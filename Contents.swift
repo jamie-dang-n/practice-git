c1ce5613bee7777b6f7394a22378fddb915355e1
Anika <pikainka@gmail.com>
2019-07-08 11:53:22 -0700

    First commit

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//var name1 = "Sabyatha"
+//var name2 = "Nailah"
+//
+//
+//print("Hi \(name1) and \(name2)! We are so happy that you're here at KWK Portland!")
+
+//let string1 = "Hello"
+//let string2 = "there"
+//var welcome = "\(string1) \(string2)"
+//print(welcome)
+//intention: (purpose, code affected, possible new into)
+//data types involved: strings
+
+//Intention: Working with integers and doubles
+//Data types: Ints and Doubles
+//
+//var int = 4
+//var double = 5.0
+//
+//print(Double(int) * double)
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+print((a+b+c+d)/4)
+print(a + b * c/d)
+print((a + b) * c/d)
